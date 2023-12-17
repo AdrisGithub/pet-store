e4fb5cc0d9516df7e4f2adceaa6a1daf7c64d738
Adrian John <adrianjohn6543@gmail.com>
2023-12-18 00:03:38 +0100

    add a basic showcase

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,73 @@
-use std::io::Write;
-use std::net::SocketAddr;
-use std::str::FromStr;
+use std::hash::Hash;
 
+use aul::level::Level;
+use wbsl::error::WBSLError;
+use wbsl::methods::Methods::{Get, Post};
 use wbsl::server::Server;
-use whdp::resp_presets::ok;
+use whdp::{Request, Response};
+use whdp::resp_presets::{bad_request, created, ok};
+use wjp::{map, ParseError, Serialize, SerializeHelper, Values};
 
-fn main() {
-    let server = Server::try_from(SocketAddr::from_str("0.0.0.0:6969").unwrap());
+#[derive(Debug)]
+struct Idk {
+    message: String,
+    code: u16,
+}
 
-    if server.is_ok() {
-        let server = server.unwrap();
-        for mut request in server {
-           let _ =  request.1.write_all(ok("IT Works...".to_string()).to_string().as_bytes());
-        }
-    } else {
-        println!("{}",server.err().unwrap())
+impl Serialize for Idk {
+    fn serialize(&self) -> Values {
+        Values::Struct(map!(("message",self.message.serialize()),("code",self.code.serialize())))
     }
 }
 
+impl TryFrom<Values> for Idk {
+    type Error = ParseError;
+    fn try_from(value: Values) -> Result<Self, Self::Error> {
+        let mut struc = value.get_struct().ok_or(ParseError::new())?;
+        let message = struc.get_val_opt("message", |val| val.get_string()).ok_or(ParseError::new())?;
+        let code = struc.map_val("code", u16::try_from)?;
+        Ok(Self {
+            code,
+            message,
+        })
+    }
+}
+
+pub static mut MAP: Vec<Idk> = Vec::new();
+
+fn main() -> Result<(), WBSLError> {
+    Server::builder()
+        .with_auto_headers("pet-store", "application/json")
+        .with_logging(Level::INFO)
+        .route("/", Get(unsafe_get))
+        .route("/", Post(unsafe_insert))
+        .listen("0.0.0.0:6969")?
+        .start();
+    Ok(())
+}
+
+pub fn unsafe_get(_req: Request) -> Response {
+    unsafe { get_all() }
+}
+
+pub fn unsafe_insert(req: Request) -> Response {
+    unsafe { insert(req) }
+}
+
+pub unsafe fn get_all() -> Response {
+    println!("{:?}", MAP);
+    ok(MAP.json())
+}
+
+pub unsafe fn insert(req: Request) -> Response {
+    let res = req.get_parsed_body();
+    if res.is_ok() {
+        let res = res.unwrap();
+        println!("{:?}", res);
+        MAP.push(res);
+        println!("{:?}", MAP);
+        created(String::new())
+    } else {
+        bad_request(String::new())
+    }
+}
